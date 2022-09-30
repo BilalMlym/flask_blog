3d7aa32884c244d45fd69de78d8c2f0d56be7f56
Bilal Mülayim <bilalmulayim@Bilal-MacBook-Pro.local>
2022-09-30 16:24:55 +0300

    processing 6th lesson

--- App/forms.py
+++ App/forms.py
@@ -1,6 +1,8 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from App.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +15,18 @@ class RegistrationForm(FlaskForm):
         'Confirm Password', validators=[DataRequired(), EqualTo('password')]
     )
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user=User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError('That username has been taken')
+
+    def validate_email(self, email):
+        user=User.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('That email has been taken')
 
 
 class LoginForm(FlaskForm):
