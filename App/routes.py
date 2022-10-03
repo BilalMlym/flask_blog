b10cf2c030aa4dba42a6a34c9d56a51bcf711648
Bilal Mülayim <bilalmulayim@Bilal-MBP.home>
2022-10-03 03:35:47 +0300

    completed 7th lesson

--- App/routes.py
+++ App/routes.py
@@ -1,4 +1,7 @@
+import os
 import email
+import secrets
+from PIL import Image
 from email.mime import image
 from fileinput import filename
 from flask import render_template, url_for, flash, redirect, request
@@ -73,11 +76,34 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pictures', picture_fn)
+    
+
+    output_size = (400, 400)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+
+
+
+
+    return picture_fn
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -88,3 +114,8 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pictures/' + current_user.image_file)
     return render_template('account.html', title='Account',  image_file = image_file, form=form )
+
+
+
+
+
